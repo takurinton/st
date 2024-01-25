e5df87da188ab44f965a3c5a97624fc6a85460fc
takurinton <takurinton@takurinton.dev>
2024-01-25 15:09:28 +0900

    add other tech based by React

--- src/main.rs
+++ src/main.rs
@@ -44,9 +44,56 @@ async fn get_technologies(url: &str) -> Result<HashSet<String>, reqwest::Error> 
 
     let mut technologies = HashSet::new();
 
-    if document.find(Name("script")).any(|n| n.attr("src").unwrap_or("").contains("next")) {
+    // Next.js
+    // <script id="__NEXT_DATA__" type="application/json"> があるかどうかで判断
+    // MEMO: SSG, SSR, app router, or CSR の区別はこれだけではできないe
+    if document
+        .find(Name("script"))
+        .any(|n| n.attr("id").unwrap_or("") == "__NEXT_DATA__")
+    {
         technologies.insert("Next.js".to_string());
     }
+
+    // <script src="/_next/static" があるかどうかで判断（前方一致）
+    if document
+        .find(Name("script"))
+        .any(|n| n.attr("src").unwrap_or("").starts_with("/_next/static"))
+    {
+        technologies.insert("Next.js".to_string());
+    }
+
+    // React
+    let mut js_urls = document
+        .find(Name("script"))
+        .filter_map(|n| n.attr("src"))
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>();
+
+    for js_url in js_urls.iter_mut() {
+        if !js_url.starts_with("http") {
+            js_url.insert_str(0, url);
+        }
+    }
+
+    let mut react = false;
+    for js_url in js_urls {
+        let js = reqwest::get(&js_url).await?.text().await?;
+        if js.contains("@license React") {
+            react = true;
+            break;
+        }
+    }
+
+    if react {
+        technologies.insert("React".to_string());
+    }
+
+    // gatsby
+    // <div id="___gatsby"> があるかどうかで判断
+    if document.find(Name("div")).any(|n| n.attr("id").unwrap_or("") == "___gatsby") {
+        technologies.insert("Gatsby".to_string());
+    }
+
     // and more
 
     Ok(technologies)
